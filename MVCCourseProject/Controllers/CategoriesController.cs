5b81ca427cf426957d2b3974c49cdf1b5bf49935
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-04 20:26:57 +0300

    Add Categories Controller and CategoryViewModel

---
+++ MVCCourseProject/Controllers/CategoriesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCourseProject.Helpers;
+using MVCCourseProject.Models;
+using DataAccess;
+using Repositories;
+
+
+namespace MVCCourseProject.Controllers
+{
+    [CustomAuthorize]
+    public class CategoriesController : Controller
+    {
+
+        UnitOfWork uow = new UnitOfWork();
+
+        public ActionResult EditCategories()
+        {
+            CategoryViewModel model = new CategoryViewModel(uow.CategoryRepository.GetAll());
+            return View(model);
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            Category category = uow.CategoryRepository.GetByID(id);
+            uow.CategoryRepository.DeleteByID(id);
+            uow.CategoryRepository.AdjustCategories(category.lft, category.rgt, false);
+            uow.Save();
+            return RedirectToAction("EditCategories");
+        }
+
+        [HttpGet]
+        public ActionResult EditCategory(int id)
+        {
+            CategoryViewModel model = new CategoryViewModel(uow.CategoryRepository.GetByID(id));
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditCategory(CategoryViewModel category)
+        {
+
+            Category dbCategory = uow.CategoryRepository.GetByID(category.ID);
+            dbCategory.Name = category.Name;
+
+            uow.CategoryRepository.Update(dbCategory, c => c.ID == category.ID);
+
+            uow.Save();
+
+            return RedirectToAction("EditCategories");
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            ViewBag.AllCategories = new SelectList(uow.CategoryRepository.GetAll(), "rgt", "Name");
+            Category category = new Category();
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            uow.CategoryRepository.AdjustCategories(category.lft, category.rgt, true);
+            uow.CategoryRepository.Save(category);
+
+            uow.Save();
+
+            return RedirectToAction("EditCategories");
+        }
+
+    }
+}
---
+++ MVCCourseProject/Models/CategoryViewModel.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCCourseProject.Models
+{
+
+    public class CategoriesViewModel
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int lft { get; set; }
+        public int rgt { get; set; }
+    }
+
+    public class CategoryViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int lft { get; set; }
+        public int rgt { get; set; }
+
+        public List<CategoriesViewModel> Categories { get; set; }
+
+        public CategoryViewModel()
+        {
+            Categories = new List<CategoriesViewModel>();
+        }
+
+        public CategoryViewModel(Category category)
+        {
+            this.ID = category.ID;
+            this.Name = category.Name;
+            this.lft = category.lft;
+            this.rgt = category.rgt;
+        }
+
+        public CategoryViewModel(List<Category> categories)
+            : this()
+        {
+            foreach (Category category in categories)
+            {
+                CategoriesViewModel item = new CategoriesViewModel();
+                item.ID = category.ID;
+                item.Name = category.Name;
+                item.lft = category.lft;
+                item.rgt = category.rgt;
+                Categories.Add(item);
+            }
+        }
+    }
+}
