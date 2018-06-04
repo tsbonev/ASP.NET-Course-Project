1b4dbd9e8e5c877b46978df0efb69bbce37e696e
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-04 19:47:31 +0300

    Add Home views

--- MVCCourseProject/Controllers/HomeController.cs
+++ MVCCourseProject/Controllers/HomeController.cs
@@ -11,11 +11,45 @@ namespace MVCCourseProject.Controllers
 {
     public class HomeController : Controller
     {
+        UnitOfWork uow = new UnitOfWork();
+
         // GET: Home
         public ActionResult Index()
         {
 
             return View();
         }
+
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Sidebar()
+        {
+            List<Category> categories = uow.CategoryRepository.GetAll();
+            HomeViewModel model = new HomeViewModel(categories);
+
+            return PartialView("_Sidebar", model);
+        }
+
+        public ActionResult Car()
+        {
+            List<Story> stories = uow.StoryRepository.GetAll();
+            HomeViewModel model = new HomeViewModel(stories);
+
+            return PartialView("_Car", model);
+        }
+
     }
 }
