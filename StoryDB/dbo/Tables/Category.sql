282190588e487ed687d0b661552671d15c1dde2e
Tsvetozar Bonev <tsbonev@gmail.com>
2018-06-05 20:44:58 +0300

    Update gitignore again

--- StoryDB/dbo/Tables/Category.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Category] (
-    [ID]   INT            IDENTITY (1, 1) NOT NULL,
-    [Name] NVARCHAR (MAX) NOT NULL,
-    [lft]  INT            NOT NULL,
-    [rgt]  INT            NOT NULL,
-    CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
